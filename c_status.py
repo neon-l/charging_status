ba8e4892a93ff3df4bfef86861d9f6d6e774d1b9
neon-l <57247635+neon-l@users.noreply.github.com>
2024-10-23 22:47:20 +0630

    Update c_status.py

--- c_status.py
+++ c_status.py
@@ -8,6 +8,8 @@ while True:
         print("Charger connected")
     else:
         print ("Charger not connected")
+        # subprocess.call("shutdown -s -t 0", shell=True) <== This will shutdown your pc.
+        # For Linux/Mac, use: subprocess.call("shutdown -h now", shell=True)
     time.sleep(3)
     subprocess.call("cls",shell=True)
     # You can use subprocess.call("clear", shell=True) for linux distros
